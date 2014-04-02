6187c8aeabb31f1a4d5641724d62b2a6cf081ecb
hdc <h.dc@sohu.com>
2014-04-02 23:02:49 +0800

    test

---
+++ src/main/java/com/sky/framework/dto/CommonsDto.java
@@ -0,0 +1,13 @@
+package com.sky.framework.dto;
+
+public class CommonsDto {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
